3a20e03a57170f04d02ee7fafacf51d55006c289
AnhagaBallas <102907803+AnhagaBallas@users.noreply.github.com>
2023-07-01 21:11:05 +0300

    Update RunnableForServer.java

--- src/main/java/RunnableForServer.java
+++ src/main/java/RunnableForServer.java
@@ -1,6 +1,5 @@
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.nio.file.Files;
@@ -9,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class RunnableForServer implements Runnable {
+    private static final String GET = "GET";
+    private static final String POST = "POST";
     final private Socket socket;
 
     public RunnableForServer(Socket socket) {
@@ -24,58 +25,69 @@ public class RunnableForServer implements Runnable {
             // read only request line for simplicity
             // must be in form GET /path HTTP/1.1
             final var requestLine = in.readLine();
+            Request request = Request.createRequest(requestLine);
             final var parts = requestLine.split(" ");
 
-            if (parts.length != 3) {
-                socket.close();
-            }
-
             final var path = parts[1];
-            if (!validPaths.contains(path)) {
-                out.write((
-                        "HTTP/1.1 404 Not Found\r\n" +
-                                "Content-Length: 0\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                out.flush();
-                socket.close();
-            }
+            if (request.getMethod().equals(GET)) {
+                if (Server.getGetMap().containsKey(request.getPath())) {
+                    Server.getGetMap().get(request.getPath()).handle(request, out);
 
-            final var filePath = Path.of(".", "public", path);
-            final var mimeType = Files.probeContentType(filePath);
 
-            // special case for classic
-            if (path.equals("/classic.html")) {
-                final var template = Files.readString(filePath);
-                final var content = template.replace(
-                        "{time}",
-                        LocalDateTime.now().toString()
-                ).getBytes();
+                }
+            } else if (request.getMethod().equals(POST)) {
+                if (Server.getPostMap().containsKey(request.getPath())) {
+                    Server.getPostMap().get(request.getPath()).handle(request, out);
+
+                }
+            } else {
+                if (!validPaths.contains(path)) {
+                    out.write((
+                            "HTTP/1.1 404 Not Found\r\n" +
+                                    "Content-Length: 0\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.flush();
+                    socket.close();
+                }
+
+                final var filePath = Path.of(".", "public", path);
+                final var mimeType = Files.probeContentType(filePath);
+
+                // special case for classic
+                if (path.equals("/classic.html")) {
+                    final var template = Files.readString(filePath);
+                    final var content = template.replace(
+                            "{time}",
+                            LocalDateTime.now().toString()
+                    ).getBytes();
+                    out.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + content.length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.write(content);
+                    out.flush();
+                    socket.close();
+                }
+
+                final var length = Files.size(filePath);
                 out.write((
                         "HTTP/1.1 200 OK\r\n" +
                                 "Content-Type: " + mimeType + "\r\n" +
-                                "Content-Length: " + content.length + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
                                 "Connection: close\r\n" +
                                 "\r\n"
                 ).getBytes());
-                out.write(content);
+                Files.copy(filePath, out);
                 out.flush();
-                socket.close();
             }
-
-            final var length = Files.size(filePath);
-            out.write((
-                    "HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: " + mimeType + "\r\n" +
-                            "Content-Length: " + length + "\r\n" +
-                            "Connection: close\r\n" +
-                            "\r\n"
-            ).getBytes());
-            Files.copy(filePath, out);
-            out.flush();
-        } catch (IOException ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 }
+
